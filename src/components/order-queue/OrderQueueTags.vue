6d971b95f9e798137c57b777f236a9a8e9374a7b
Doug Richar <drichar@gmail.com>
2018-08-03 00:47:13 -0400

    Upload files for tags

--- src/components/order-queue/OrderQueueTags.vue
+++ src/components/order-queue/OrderQueueTags.vue
@@ -9,9 +9,21 @@
     </b-card-header>
     <b-collapse id="OrderQueueTags" v-model="showCollapse" accordion="my-accordion" role="tabpanel">
       <b-card-body>
-        <div class="row mb-4">
+        <div class="row mb-3">
           <div class="col">
             <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col">
+            <button class="btn btn-primary">
+              Add Tag
+            </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <b-form-file v-model="files" :state="Boolean(files)" multiple placeholder="Select/drop files"></b-form-file>
           </div>
         </div>
       </b-card-body>
@@ -24,7 +36,8 @@ export default {
   name: 'OrderQueueTags',
   data () {
     return {
-      showCollapse: false
+      showCollapse: false,
+      files: null
     }
   }
 }
