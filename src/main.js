3d5c82cf84ac641f675fd565079c45047f912c05
Doug Richar <drichar@gmail.com>
2018-08-04 14:38:17 -0400

    Font Awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.use(BootstrapVue)
 
